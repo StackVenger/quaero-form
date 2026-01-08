d4c71df0882f9bd91c4217d2812a73f0eae40e95
nazim <nazimdev10022001@gmail.com>
2026-01-08 22:39:35 +0600

    🎉 initial: create project

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { Button } from "@/components/ui/button"
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className='text-textPrimary'>Hello 
+
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
+
+
+export default App</div>
+  )
+}
+
+export default App
